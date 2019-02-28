ce94d92433fa28a8e53738e51c90d5e2bba508b3
Stewart Lawson <stewartlwson@gmail.com>
2019-02-28 03:27:43 +0000

    seperated io functions into seperate class outputted 12 bar blues data as json file

---
+++ util/io.py
@@ -0,0 +1,42 @@
+import json
+import hashlib
+
+class IO:
+    def __init__(self):
+        pass
+
+    def load_json(self, path):
+        data = {}
+        with open(path, "r") as infile:
+            data = json.load(infile)
+        return data
+
+    def save_json(self, path, data):
+        with open(path, "w") as outfile:
+            json.dump(data, outfile)
+
+    def load_training_data(self, filename):
+        path = "./training data/" + filename + ".json"
+        data = self.load_json(path)
+        return data["data"]
+
+    def save_training_data(self, filename, data):
+        path = "./training data/" + filename + ".json"
+        data = {
+            "data": data
+        }
+        self.save_json(path, data)
+
+    def load_song(self, filename):
+        path = "./songs/" + filename + ".json"
+        data = self.load_json(path)
+        return data["melody"], data["comp"]
+
+    def save_song(self, filename, melody, comp):
+        filename = str(hashlib.sha1(str(filename).encode("utf-8")).hexdigest())
+        path = "./songs/" + filename + ".json"
+        data = {
+            "melody": melody,
+            "comp": comp,
+        }
+        self.save_json(path, data)
